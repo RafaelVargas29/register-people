44c129001ca8369f218b8dd0dbafea8f5636901b
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-09-29 16:11:14 -0300

    Layout v1

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body class="site">
+    <header id="site-header">
+
+    </header>
+    
+    <div id="navbar">
+        <ul class="navbar-nav">
+            <li class="navbar-item">
+                <a href="#" class="navbar-link">Cadastrar Pessoa</a>
+            </li>
+            <li class="navbar-item">
+                <a href="#" class="navbar-link">Modificar Pessoa</a>
+            </li>
+            <li class="navbar-item">
+                <a href="#" class="navbar-link">Listar Pessoas</a>
+            </li>
+            <li class="navbar-item">
+                <a href="#" class="navbar-link">Deletar Pessoa</a>
+            </li>
+        </ul>
+    </div>    
+
+    <footer id="site-content-footer">
+        <p>Register People &copy; 2022</p>
+    </footer>
+</body>
+</html>
